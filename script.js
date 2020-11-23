e6b0e697170d3e5cec9cdee24aa1d204da868fc5
MZ <nikita.zholkovskyi@gmail.com>
2020-11-23 18:53:36 +0100

    added flipcard func

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const images = document.getElementsByClassName("front-image");
+const images = document.getElementsByClassName("back-image");
+const cards = document.querySelectorAll(".cards");
 
 function loader() {
     document.addEventListener('DOMContentLoaded', function () {
@@ -7,5 +8,23 @@ function loader() {
         }
     }, false);
 }
+
+
+const flippedCards = [];
+
+function flipCard() {
+
+    if (flippedCards.length < 2) {
+        this.classList.add('flip');
+        let flippedCard = this;
+        flippedCards.push(flippedCard);
+    }
+
+    if (flippedCards.length === 2) {
+        console.log(flippedCards[0] === flippedCards[1])
+    }
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
 
 loader();
